abf50a17ac25ddeaeda49e6b334454307a90c038
giochagasb <gcbrujas@gmail.com>
2022-04-21 21:54:18 -0300

    Atualização de paginas e pastas

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <title>Projeto Intergador 2 Semestre</title>
+    </head>
+
+    <body>
+        <h1>Primeira pagina.</h1>
+        <h3>Depois fazer alterações.</h3>
+    </body>
+
+
+    
---
+++ index.php
@@ -0,0 +1,2 @@
+<h1>Primeira pagina.</h1>
+<h3>Depois fazer alterações.</h3>
