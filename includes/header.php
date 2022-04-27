834fc0ff00b8e5ff26b2622e90327111416076bf
giochagasb <gcbrujas@gmail.com>
2022-04-27 11:02:42 -0300

    Base header feita

--- includes/header.php
+++ includes/header.php
@@ -7,15 +7,18 @@
         
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" href="../css/bootstrap.min.css"> -->
+        <link rel="stylesheet" href="../css/campeoes.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
 
         <title>Projeto Intergador 2 Semestre</title>
     </head>
 
     <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-dark">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="https://www.figma.com/file/NjXOxe2egn0qcqeeBXOwki/Estrutura-de-Dados?node-id=0%3A1">Teste</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
@@ -28,32 +31,19 @@
                 </div>
                 </div>
             </div>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="offcanvasNavbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="offcanvasNavbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-        </ul>
-          
+            <div class="btn-group">
+              <button type="button" class="btn btn-secondary dropdown-toggle" id="dropdownMenuButton2" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
+                Partidos
+              </button>
+              <ul class="dropdown-menu dropdown-menu-lg-end" aria-labelledby="dropdownMenuButton2">
+                <li><a class="dropdown-item" href="https://pt.org.br/">PT</a></li>
+                <li><a class="dropdown-item" href="https://psol50.org.br/">SOL</a></li>
+                <li><a class="dropdown-item" href="#">PSDB</a></li>
+                <li><a class="dropdown-item" href="#">Avnate</a></li>
+              </ul>
+            </div>
         </nav>
         
-
-
         
     </header>
 
--- index.php
+++ index.php
@@ -1,2 +1,4 @@
+<?php include_once('includes/header.php'); ?>
+
 <h1>Primeira pagina.</h1>
 <h3>Depois fazer alterações.</h3>
