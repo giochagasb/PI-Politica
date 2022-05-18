a9c9d890db81b44ed57ba5afd6a49797a359d81f
giochagasb <gcbrujas@gmail.com>
2022-05-18 11:51:18 -0300

    18/05

--- includes/footer.php
+++ includes/footer.php
@@ -16,7 +16,6 @@
             
           </section>
 
-          <section class="vertical col-1 col-sm-1 col-md-1"></section>
 
           <section class="col-10 col-sm-10 col-md-4 ">
 
--- pages/candidatos.php
+++ pages/candidatos.php
@@ -1,6 +1,115 @@
 <?php include_once('../includes/header.php'); ?>
 
-<h1>Candidatos.</h1>
-<h3>Depois fazer alterações.</h3>
+<main class="container py-5">
+
+  <article class="row justify-content-center align-items-center">
+    <h2 class="text-center"><b>Conheça abaixo os pré candidatos a presidencia do Brasil em 2023.</b></h2>
+
+
+    <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/janones.jpg" class="card-img-top" alt="Imagem do candidato André Janones.">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/ciro.jpg" class="card-img-top" alt="Imagem do candidato Ciro Gomes.">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a short card.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/leite.jpg" class="card-img-top" alt="Imagem do candidato Eduardo Leite">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/eymael.jpg" class="card-img-top" alt="Imagem do candidato Eymael.">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/janones.jpg" class="card-img-top" alt="Imagem do candidato André Janones.">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/ciro.jpg" class="card-img-top" alt="Imagem do candidato Ciro Gomes.">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a short card.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/leite.jpg" class="card-img-top" alt="Imagem do candidato Eduardo Leite">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/eymael.jpg" class="card-img-top" alt="Imagem do candidato Eymael.">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/leite.jpg" class="card-img-top" alt="Imagem do candidato Eduardo Leite">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/leite.jpg" class="card-img-top" alt="Imagem do candidato Eduardo Leite">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+        <div class="col" style="width: 300px;">
+            <div class="card h-100">
+            <img src="../img/leite.jpg" class="card-img-top" alt="Imagem do candidato Eduardo Leite">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+            </div>
+        </div>
+    </div>
+
+  </article>
+
+</main>
 
 <?php include_once('../includes/footer.php'); ?>
