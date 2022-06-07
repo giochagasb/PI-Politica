0cc1e6842afc92b8d1dd5841d98f50df013fb132
giochagasb <gcbrujas@gmail.com>
2022-06-07 11:32:06 -0300

    07/06

--- pages/cidadania.php
+++ pages/cidadania.php
@@ -1,6 +1,63 @@
 <?php include_once('../includes/header.php'); ?>
 
-<h1>Cidadania.</h1>
-<h3>Depois fazer alterações.</h3>
+<main class="container-fluid text-justify">
+  <!--container1-->
+  <div class="container">
+    <article class="row justify-content-center">
+      <div class="col-10 col-sm-12 col-md-10 col-lg-11">
+        <h2 class="text-center mb-3 fontenegrito">IMPACTO DO COVID</h2>
+        <p class="fontteste">A pandemia do coronavírus assumiu no Brasil a dimensão de uma catástrofe humanitária.
+          Centenas de milhares de pessoas perderam a vida, outras tantas apresentam as sequelas de uma lenta
+          recuperação, e milhões foram levados ao desemprego e à faixa da pobreza extrema. </p>
+        <p>O drama se aprofunda pelo fato de que experimentamos quatro grandes crises simultâneas e interconectadas:
+          sanitária, econômica, política e comportamental. São diversos os fatores que as retroalimentam, tais como o
+          negacionismo, a necropolítica, a gestão pública mórbida e ineficiente, a indiferença com o próximo e a falta
+          de coordenação no nível federal. </p>
+        <p>A crueldade da pandemia de Covid-19 tem deixado marcas profundas na história, com quase 4 milhões de vidas
+          perdidas. Em nenhuma das crises sanitárias do século 21 o número de mortes foi tão grande, ou exigiu do Estado
+          a injeção de recursos financeiros em escala tão elevada com o fim de reduzir os impactos econômicos e o
+          agravamento das desigualdades sociais. </p>
+      </div>
+    </article>
+  </div>
+  <!--fim container1-->
+
+    
+
+    
+
+  <!--container3-->
+  <div class="container">
+    <div class="row mb-3 py-4 align-items-center justify-content-center">
+      <article class="col-10 col-sm-12 col-md-10 col-lg-6 order-lg-1">
+        <h2 class="text-center mb-3 fontenegrito">MERCADO DE TRABALHO</h2>
+
+        <p> Os trabalhadores mais afetados foram aqueles com menor grau de instrução que atuam em subocupações &#40;que são
+          os que trabalham menos de 40 horas em suas ocupações, e que gostariam de trabalhar mais horas&#41; ou como
+          informais. A forte precarização das relações trabalhistas, anterior à crise pandêmica, favoreceu a adoção do
+          regime de trabalho intermitente na economia brasileira, uma modalidade na qual o funcionário trabalha apenas
+          algumas horas por semana, o que não apenas reduz a jornada de trabalho, mas também implica significativas
+          perdas de renda.</p>
+        <p>Já a análise da crise sob a perspectiva de gênero revela que os custos da pandemia recaíram
+          mais fortemente sobre as mulheres do que sobre os homens. O fechamento das escolas e das creches, que
+          viabilizavam a dupla jornada, impôs às mulheres o cuidado dos filhos, assim como a necessidade de cuidados com
+          os idosos da família. A pandemia trouxe efeitos devastadores para a população economicamente mais vulnerável,
+          assalariada ou que exercia trabalhos informais, na qual o percentual de negros é expressivo. Nesse sentido,
+          essa população foi duplamente afetada, pela perda da renda e pela impossibilidade de fazer o distanciamento
+          social.</p>
+      </article>
+
+      <aside class="col-10 col-sm-10 col-md-10 col-lg-5 order-lg-0">
+        <img src="imagens/trabalho" class="imglargura rounded-3"
+          alt="graficos de casos registrados na Itália, desde a abril de 2020 até novembro de 2021.">
+      </aside>
+
+
+
+    </div>
+  </div>
+  <!--fimcontainer3-->
+
+</main>
 
 <?php include_once('../includes/footer.php'); ?>
