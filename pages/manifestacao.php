250fc85d4c38cdf0429c66844be4d1394d03358b
Felipe <goesfelipesantos@gmail.com>
2022-06-11 00:15:14 -0300

    Pagina Manifestações

--- pages/manifestacao.php
+++ pages/manifestacao.php
@@ -5,7 +5,7 @@
   <div class="container">
     <article class="row justify-content-center">
       <div class="col-10 col-sm-12 col-md-10 col-lg-11">
-        <h2 class="text-center mb-3 fontenegrito">IMPACTO DO COVID</h2>
+        <h2 class="text-center mb-3 fontenegrito"> Manifestações Contra o corte de verbas da educação</h2>
         <p class="fontteste">A pandemia do coronavírus assumiu no Brasil a dimensão de uma catástrofe humanitária.
           Centenas de milhares de pessoas perderam a vida, outras tantas apresentam as sequelas de uma lenta
           recuperação, e milhões foram levados ao desemprego e à faixa da pobreza extrema. </p>
@@ -13,6 +13,7 @@
           sanitária, econômica, política e comportamental. São diversos os fatores que as retroalimentam, tais como o
           negacionismo, a necropolítica, a gestão pública mórbida e ineficiente, a indiferença com o próximo e a falta
           de coordenação no nível federal. </p>
+          <img src="../img/manifestacao1.jpg"  alt="Cartaz escrito com a frase: De tanto poupar em educação, firaremos ricos em ignorância. ">
         <p>A crueldade da pandemia de Covid-19 tem deixado marcas profundas na história, com quase 4 milhões de vidas
           perdidas. Em nenhuma das crises sanitárias do século 21 o número de mortes foi tão grande, ou exigiu do Estado
           a injeção de recursos financeiros em escala tão elevada com o fim de reduzir os impactos econômicos e o
@@ -25,7 +26,7 @@
   <div class="container">
     <article class="row justify-content-center">
       <div class="col-10 col-sm-12 col-md-10 col-lg-11">
-        <h2 class="text-center mb-3 fontenegrito">IMPACTO DO COVID</h2>
+        <h2 class="text-center mb-3 fontenegrito">Ato em defesa da democracia</h2>
         <p class="fontteste">A pandemia do coronavírus assumiu no Brasil a dimensão de uma catástrofe humanitária.
           Centenas de milhares de pessoas perderam a vida, outras tantas apresentam as sequelas de uma lenta
           recuperação, e milhões foram levados ao desemprego e à faixa da pobreza extrema. </p>
@@ -33,6 +34,7 @@
           sanitária, econômica, política e comportamental. São diversos os fatores que as retroalimentam, tais como o
           negacionismo, a necropolítica, a gestão pública mórbida e ineficiente, a indiferença com o próximo e a falta
           de coordenação no nível federal. </p>
+          <img src="../img/manifestacao2.jpg" alt="">
         <p>A crueldade da pandemia de Covid-19 tem deixado marcas profundas na história, com quase 4 milhões de vidas
           perdidas. Em nenhuma das crises sanitárias do século 21 o número de mortes foi tão grande, ou exigiu do Estado
           a injeção de recursos financeiros em escala tão elevada com o fim de reduzir os impactos econômicos e o
@@ -40,9 +42,48 @@
       </div>
     </article>
   </div>
+
   <!--fim container1-->
 
+  <div class="container">
+    <article class="row justify-content-center">
+      <div class="col-10 col-sm-12 col-md-10 col-lg-11">
+        <h2 class="text-center mb-3 fontenegrito">Manifestações contra inflação e aumentos dos preços dos alimentos</h2>
+        <p class="fontteste">A pandemia do coronavírus assumiu no Brasil a dimensão de uma catástrofe humanitária.
+          Centenas de milhares de pessoas perderam a vida, outras tantas apresentam as sequelas de uma lenta
+          recuperação, e milhões foram levados ao desemprego e à faixa da pobreza extrema. </p>
+        <p>O drama se aprofunda pelo fato de que experimentamos quatro grandes crises simultâneas e interconectadas:
+          sanitária, econômica, política e comportamental. São diversos os fatores que as retroalimentam, tais como o
+          negacionismo, a necropolítica, a gestão pública mórbida e ineficiente, a indiferença com o próximo e a falta
+          de coordenação no nível federal. </p>
+          <img src="../img/manifestacao3.jpg" alt="">
+        <p>A crueldade da pandemia de Covid-19 tem deixado marcas profundas na história, com quase 4 milhões de vidas
+          perdidas. Em nenhuma das crises sanitárias do século 21 o número de mortes foi tão grande, ou exigiu do Estado
+          a injeção de recursos financeiros em escala tão elevada com o fim de reduzir os impactos econômicos e o
+          agravamento das desigualdades sociais. </p>
+      </div>
+    </article>
+  </div>
 
+  <div class="container">
+    <article class="row justify-content-center">
+      <div class="col-10 col-sm-12 col-md-10 col-lg-11">
+        <h2 class="text-center mb-3 fontenegrito">Manifestações no Lollapalooza contra o governo Bolsonaro</h2>
+        <p class="fontteste">A pandemia do coronavírus assumiu no Brasil a dimensão de uma catástrofe humanitária.
+          Centenas de milhares de pessoas perderam a vida, outras tantas apresentam as sequelas de uma lenta
+          recuperação, e milhões foram levados ao desemprego e à faixa da pobreza extrema. </p>
+        <p>O drama se aprofunda pelo fato de que experimentamos quatro grandes crises simultâneas e interconectadas:
+          sanitária, econômica, política e comportamental. São diversos os fatores que as retroalimentam, tais como o
+          negacionismo, a necropolítica, a gestão pública mórbida e ineficiente, a indiferença com o próximo e a falta
+          de coordenação no nível federal. </p>
+          <img src="../img/manifestacao4.jpg" alt="">
+        <p>A crueldade da pandemia de Covid-19 tem deixado marcas profundas na história, com quase 4 milhões de vidas
+          perdidas. Em nenhuma das crises sanitárias do século 21 o número de mortes foi tão grande, ou exigiu do Estado
+          a injeção de recursos financeiros em escala tão elevada com o fim de reduzir os impactos econômicos e o
+          agravamento das desigualdades sociais. </p>
+      </div>
+    </article>
+  </div>
     
 
     
