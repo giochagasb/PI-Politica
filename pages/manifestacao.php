8a7c3a0b59ee781bb0622f5efbe1d7843cb81960
Felipe <goesfelipesantos@gmail.com>
2022-06-13 11:11:34 -0300

    alteração titulo noticias

--- pages/manifestacao.php
+++ pages/manifestacao.php
@@ -1,19 +1,23 @@
 <?php include_once('../includes/header.php'); ?>
 
-<main class="manifestacao" class="container-fluid text-justify">
+<main class="container-fluid text-justify">
   <!--container1-->
   <div class="container">
     <article class="row justify-content-center">
-      <div class="manifestacao"  class="col-10 col-sm-12 col-md-10 col-lg-11">
+      <div class="col-10 col-sm-12 col-md-10 col-lg-11">
         <h2 class="text-center mb-3 fontenegrito"> Manifestações Contra o corte de verbas da educação</h2>
-        <div class="center">
-          <img  class="img-manifestacao" src="../img/manifestacao1.jpg"  alt="Cartaz escrito com a frase: De tanto poupar em educação, firaremos ricos em ignorância. ">
-        </div>
-        <div class="testando">
-          <p  class="fontteste">Em 2019 milhares de pessoas, em sua maioria estudantes e professores, saíram às ruas para protestar contra o anúncio da Secretaria de Educação de cortes nos orçamentos discricionários de universidades e órgãos federais em 26 estados e no DF. Escolas e universidades públicas e privadas também aderiram aos protestos. União Nacional dos Estudantes (UNE) convoca nova greve nacional no dia 30 </p>
-          <p>As manifestações são resultado de um processo de atrito entre o Executivo federal e as instituições de ensino superior que começou com o governo de Jair Bolsonaro. O presidente em Dallas classificou os manifestantes como "idiotas úteis". e; massa de manobra; .</p>
-          <p>No DF, pelo menos seis mil pessoas marcharam para a Esplanada dos Ministérios. Em São Paulo (SP), milhares ocuparam a Avenida Paulista. No Rio de Janeiro, um grupo de pessoas caminha da Candelária até a região central do Brasil. A operação foi pacífica, mas, eventualmente, um grupo de homens mascarados entrou em confronto com a polícia. Os policiais militares responderam com bombas de gás.. </p>
-        </div>
+        <p class="fontteste">A pandemia do coronavírus assumiu no Brasil a dimensão de uma catástrofe humanitária.
+          Centenas de milhares de pessoas perderam a vida, outras tantas apresentam as sequelas de uma lenta
+          recuperação, e milhões foram levados ao desemprego e à faixa da pobreza extrema. </p>
+        <p>O drama se aprofunda pelo fato de que experimentamos quatro grandes crises simultâneas e interconectadas:
+          sanitária, econômica, política e comportamental. São diversos os fatores que as retroalimentam, tais como o
+          negacionismo, a necropolítica, a gestão pública mórbida e ineficiente, a indiferença com o próximo e a falta
+          de coordenação no nível federal. </p>
+          <img src="../img/manifestacao1.jpg"  alt="Cartaz escrito com a frase: De tanto poupar em educação, firaremos ricos em ignorância. ">
+        <p>A crueldade da pandemia de Covid-19 tem deixado marcas profundas na história, com quase 4 milhões de vidas
+          perdidas. Em nenhuma das crises sanitárias do século 21 o número de mortes foi tão grande, ou exigiu do Estado
+          a injeção de recursos financeiros em escala tão elevada com o fim de reduzir os impactos econômicos e o
+          agravamento das desigualdades sociais. </p>
       </div>
     </article>
   </div>
@@ -21,14 +25,20 @@
   <!--container1-->
   <div class="container">
     <article class="row justify-content-center">
-      <div class="manifestacao" class="col-10 col-sm-12 col-md-10 col-lg-11">
+      <div class="col-10 col-sm-12 col-md-10 col-lg-11">
         <h2 class="text-center mb-3 fontenegrito">Ato em defesa da democracia</h2>
-        <div class="center">
-          <img class="img-manifestacao" src="../img/manifestacao2.jpg" alt="">
-        <div>
-        <p class="fontteste">Já no ano de 2020, manifestantes realizaram um protesto na tarde do domingo do dia 14/06/2020 na Avenida Paulista, no centro de São Paulo, para defender a democracia, contra o presidente Jair Bolsonaro (que não fez festa) e contra o racismo. A mostra começa às 14h em frente aos espaços abertos do Museu de Arte de São Paulo (MASP).</p>
-        <p>O grupo seguiu pela avenida em direção ao Paraíso até se aproximar da estação Brigadeiro do metrô, onde foi proferido o discurso. O ato permaneceu pacífico até o final do protesto. Por volta das 16h, começou a dispersão. </p>
-       
+        <p class="fontteste">A pandemia do coronavírus assumiu no Brasil a dimensão de uma catástrofe humanitária.
+          Centenas de milhares de pessoas perderam a vida, outras tantas apresentam as sequelas de uma lenta
+          recuperação, e milhões foram levados ao desemprego e à faixa da pobreza extrema. </p>
+        <p>O drama se aprofunda pelo fato de que experimentamos quatro grandes crises simultâneas e interconectadas:
+          sanitária, econômica, política e comportamental. São diversos os fatores que as retroalimentam, tais como o
+          negacionismo, a necropolítica, a gestão pública mórbida e ineficiente, a indiferença com o próximo e a falta
+          de coordenação no nível federal. </p>
+          <img src="../img/manifestacao2.jpg" alt="">
+        <p>A crueldade da pandemia de Covid-19 tem deixado marcas profundas na história, com quase 4 milhões de vidas
+          perdidas. Em nenhuma das crises sanitárias do século 21 o número de mortes foi tão grande, ou exigiu do Estado
+          a injeção de recursos financeiros em escala tão elevada com o fim de reduzir os impactos econômicos e o
+          agravamento das desigualdades sociais. </p>
       </div>
     </article>
   </div>
@@ -37,31 +47,40 @@
 
   <div class="container">
     <article class="row justify-content-center">
-      <div class="manifestacao" class="col-10 col-sm-12 col-md-10 col-lg-11">
+      <div class="col-10 col-sm-12 col-md-10 col-lg-11">
         <h2 class="text-center mb-3 fontenegrito">Manifestações contra inflação e aumentos dos preços dos alimentos</h2>
-        <div class="center">
-          <img  class="img-manifestacao" src="../img/manifestacao3.jpg" alt="">
-        </div>
-        <p class="fontteste">Em 2021 com o avanço da pandemia, milhares foram às ruas para protestar contra a alta inflação e o custo de vida do Brasil, o aumento dos preços dos combustíveis e a perda de empregos. O governo (Ministério da Saúde e Educação) também tem sido criticado por aumentar a fome, a miséria e a corrupção.  </p>
-        <p>Milhares de pessoas saíram às ruas neste sábado (10/02) para protestar contra o presidente Jair Bolsonaro (sem partido). </p>
-        <p>Os temas mais comuns em cartazes e discursos foram a defesa da democracia, o impeachment e as críticas à forma como o governo federal lidou com a pandemia de COVID-19 e o aumento dos preços de alimentos, gás e combustíveis.
-        </p>
-        <p>Os eventos são organizados por movimentos sociais e centrais sindicais e contam com a participação de cerca de 20 partidos políticos, associações estudantis e até torcidas organizadas.
-        </p>
+        <p class="fontteste">A pandemia do coronavírus assumiu no Brasil a dimensão de uma catástrofe humanitária.
+          Centenas de milhares de pessoas perderam a vida, outras tantas apresentam as sequelas de uma lenta
+          recuperação, e milhões foram levados ao desemprego e à faixa da pobreza extrema. </p>
+        <p>O drama se aprofunda pelo fato de que experimentamos quatro grandes crises simultâneas e interconectadas:
+          sanitária, econômica, política e comportamental. São diversos os fatores que as retroalimentam, tais como o
+          negacionismo, a necropolítica, a gestão pública mórbida e ineficiente, a indiferença com o próximo e a falta
+          de coordenação no nível federal. </p>
+          <img src="../img/manifestacao3.jpg" alt="">
+        <p>A crueldade da pandemia de Covid-19 tem deixado marcas profundas na história, com quase 4 milhões de vidas
+          perdidas. Em nenhuma das crises sanitárias do século 21 o número de mortes foi tão grande, ou exigiu do Estado
+          a injeção de recursos financeiros em escala tão elevada com o fim de reduzir os impactos econômicos e o
+          agravamento das desigualdades sociais. </p>
       </div>
     </article>
   </div>
 
   <div class="container">
     <article class="row justify-content-center">
-      <div class="manifestacao" class="col-10 col-sm-12 col-md-10 col-lg-11">
-        <h2  class="text-center mb-3 fontenegrito">Manifestações no Lollapalooza contra o governo Bolsonaro</h2>
-        <div class="center">
-          <img class="img-manifestacao" src="../img/manifestacao4.jpg" alt="">
-        </div>
-        <p class="fontteste">A edição deste ano do Lollapalooza virou palco para protestos e xingamentos contra o presidente Jair Bolsonaro (PL). Artistas que se apresentaram no festival, no sábado (26), empolgaram o público com manifestações contra o presidente da República, reforçando uma tendência iniciada pela cantora Pabllo Vittar, que no primeiro dia do evento ergueu uma bandeira em favor do pré-candidato à Presidência da República, o ex-presidente Lula, do PT, e agitou o público com gritos de “fora Bolsonaro”. </p>
-        <p>O PL, partido do presidente Jair Bolsonaro, que tenta a reeleição, protestou contra a manifestação da cantora, e, na manhã deste domingo (27), o Tribunal Superior Eleitoral (TSE) reconheceu a reclamação, e classificou como propaganda eleitoral antecipada as manifestações de Pabllo, e também da cantora britânica Marina, na sexta-feira (25). </p>
-        <p>Mas, depois disso, no segundo dia do festival, artistas como Emicida, Silva e o cantor Jão também aproveitaram o momento para promover campanha pelo voto jovem, incentivando adolescentes a tirarem seu título de eleitor: “1º, boa noite! 2º, estava com a maior saudade; 3º, se você tem de 16 a 18 anos tire seu título de eleitor e 4º, Bolsonaro vai tomar no cu”, disse Emicida ainda no início do seu show. A fala foi seguida de gritos do público. </p>
+      <div class="col-10 col-sm-12 col-md-10 col-lg-11">
+        <h2 class="text-center mb-3 fontenegrito">Manifestações no Lollapalooza contra o governo Bolsonaro</h2>
+        <p class="fontteste">A pandemia do coronavírus assumiu no Brasil a dimensão de uma catástrofe humanitária.
+          Centenas de milhares de pessoas perderam a vida, outras tantas apresentam as sequelas de uma lenta
+          recuperação, e milhões foram levados ao desemprego e à faixa da pobreza extrema. </p>
+        <p>O drama se aprofunda pelo fato de que experimentamos quatro grandes crises simultâneas e interconectadas:
+          sanitária, econômica, política e comportamental. São diversos os fatores que as retroalimentam, tais como o
+          negacionismo, a necropolítica, a gestão pública mórbida e ineficiente, a indiferença com o próximo e a falta
+          de coordenação no nível federal. </p>
+          <img src="../img/manifestacao4.jpg" alt="">
+        <p>A crueldade da pandemia de Covid-19 tem deixado marcas profundas na história, com quase 4 milhões de vidas
+          perdidas. Em nenhuma das crises sanitárias do século 21 o número de mortes foi tão grande, ou exigiu do Estado
+          a injeção de recursos financeiros em escala tão elevada com o fim de reduzir os impactos econômicos e o
+          agravamento das desigualdades sociais. </p>
       </div>
     </article>
   </div>
